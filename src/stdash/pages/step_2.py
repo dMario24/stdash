0dc25f8028646196d827a4698bca54e46c1f1195
dMario24 <data.mario24@gmail.com>
2024-09-27 12:33:57 +0900

    step2

--- src/stdash/pages/step_2.py
+++ src/stdash/pages/step_2.py
@@ -41,3 +41,23 @@ plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d %H'))
 
 # 화면에 그리기
 st.pyplot(plt)
+
+# char2
+a_counts = df.groupby('request_time').size().reset_index(name='count')
+a_counts['type'] = 'request_time'
+a_counts.rename(columns={'request_time': 'yymmdd'}, inplace=True)
+
+# B 열에 대해서 count
+b_counts = df.groupby('prediction_time').size().reset_index(name='count')
+b_counts['type'] = 'prediction_time'
+b_counts.rename(columns={'prediction_time': 'yymmdd'}, inplace=True)
+
+# A와 B count 데이터 합치기
+result = pd.concat([a_counts, b_counts]).sort_values(by='yymmdd').reset_index(drop=True)
+st.bar_chart(result, x="yymmdd", y="count", color="type")
+st.line_chart(result, x="yymmdd", y="count", color="type")
+
+
+# char2
+result_df = df.groupby(['request_time', 'request_user']).size().reset_index(name='count')
+st.bar_chart(result_df, x="request_time", y="count", color="request_user", horizontal=False, stack=False)
